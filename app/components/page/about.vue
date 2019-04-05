b582f786e3c81dc1440209582d44a0e1f3377f71
Zillion <skyestzhang@163.com>
2019-04-05 14:34:04 +0800

    add how to use

--- app/components/page/about.vue
+++ app/components/page/about.vue
@@ -5,12 +5,13 @@
     </ActionBar>
     <StackLayout class="page_body">
       <Label text="How To Use" class="left_title h1"/>
-      <Label text="1. open the app, and tap the home buton, DONT TAB the BACK button, it would close the app"
+      <Label text="1. open the app, and tap the home buton, DONT TAB the BACK button, it would close the app (Will fix it later)"
        :textWrap="true" class="text_line"/>
-      <Label text="2. open the ins" :textWrap="true" class="text_line"/>
+      <Label text="2. Press the home button to open instagram, click on the top right corner of the image you want to download, copy the link" :textWrap="true" class="text_line"/>
+      <Label text="3. Then open this app, you will see the images" class="text_line"/>
 
       <Label text="如何使用" class="left_title h1"/>
-      <Label text="1. 打开app, 不要按返回键，否则会退出app" :textWrap="true" class="text_line"/>
+      <Label text="1. 打开app, 不要按返回键，否则会退出app (后期会改进)" :textWrap="true" class="text_line"/>
       <Label text="2. 按home键，打开instagram，对需要下载的图片点击右上角，复制链接" :textWrap="true" class="text_line"/>
       <Label text="3. 打开本app，将会看到你需要的图片" :textWrap="true" class="text_line"/>
 
